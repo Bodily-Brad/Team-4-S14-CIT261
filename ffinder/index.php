f946d5281ba468af02544fbdf5d5b70aec2acfbe
Bodily-Brad <bod12002@byui.edu>
2014-06-03 19:27:29 -0600

    ffinder

---
+++ ffinder/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        FoodFinder
+        <form>
+            <label>Pizza</label><input type="checkbox">
+            <label>Pop</label><input type="checkbox">
+            <label>Sugar</label><input type="checkbox">
+            <label>Soda</label><input type="checkbox">            
+            <input type="submit" value="Go">
+        </form>
+    </body>
+</html>
