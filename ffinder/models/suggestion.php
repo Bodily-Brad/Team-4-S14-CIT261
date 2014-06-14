0e114b5dcc6575ec6ba603a7325e3f6104ab5646
Bodily-Brad <bod12002@byui.edu>
2014-06-14 09:30:21 -0600

    Added Suggestion class

---
+++ ffinder/models/suggestion.php
@@ -0,0 +1,64 @@
+<?php
+/**
+ * @author Brad Bodily
+ */
+
+/**
+ * Represents a possible suggestion, including its component qualities
+ */
+class Suggestion
+{
+    // PUBLIC MEMBERS
+    /**
+     * How the suggestion is described on screen
+     * @var string 
+     */
+    public $description;
+    /**
+     * A collection of componen qualities that describe this suggestion
+     * @var string array
+     */
+    public $qualities = array();
+    
+    // PUBLIC FUNCTIONS
+    /**
+     * Clears a quality from this Suggestion's quality list
+     * @param string $quality The name of the quality to clear
+     */
+    public function ClearQuality($quality)
+    {
+        if (array_key_exists($this->qualities, $quality))
+        {
+            unset($this->qualities[$quality]);            
+        }
+    }
+    
+    /**
+     * Calculates a rank based on similarity to the specified qualities
+     * @param string array $compareQualities Qualities to compare against
+     * @return int quality rank
+     */
+    public function GetQualityRank($compareQualities)
+    {
+        $count = 0;
+        
+        foreach ($compareQualities as $key => $value)
+        {
+            if (array_key_exists($this->qualities, $key))
+            {
+                $count++;                
+            }
+        }
+        
+        return $count;
+    }
+    
+    /**
+     * Sets a quality to this Suggestion's list of qualities
+     * @param string $quality Quality to set/add
+     */
+    public function SetQuality($quality)
+    {
+        $this->qualities[$quality] = true;
+    }
+}
