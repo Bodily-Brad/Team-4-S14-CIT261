a88c9eb971f330c7294cb286bef233327f39d010
Bodily-Brad <bod12002@byui.edu>
2014-06-25 18:26:37 -0600

    New CSS

--- ffinder/views/view_results.php
+++ ffinder/views/view_results.php
@@ -5,14 +5,16 @@
         <title></title>
     </head>
     <body>
-        <img src='/media/images/brand_glass.png'>
-        <h1>FoodFinder</h1>
-        <p><?php echo $results?></p>
-        [Map]<br>
-        <form method="post">
-            <input type="hidden" name="action" value="start"/>
-            <br>
-            <input type="submit" value="try again" class='btn'>
-        </form>
+        <span id="contentwrapper">        
+            <img src='/media/images/brand_glass.png'>
+            <h1>FoodFinder</h1>
+            <p><?php echo $results?></p>
+            [Map]<br>
+            <form method="post">
+                <input type="hidden" name="action" value="start"/>
+                <br>
+                <input type="submit" value="try again" class='btn'>
+            </form>
+        </span>
     </body>
 </html>
--- ffinder/views/view_survey.php
+++ ffinder/views/view_survey.php
@@ -5,16 +5,18 @@
         <title></title>
     </head>
     <body>
-        <img src='/media/images/brand_glass.png'>        
-        <h1>FoodFinder</h1>
-        <form method="post">
-            <label>Pizza</label><input type="checkbox">
-            <label>Pop</label><input type="checkbox">
-            <label>Sugar</label><input type="checkbox">
-            <label>Soda</label><input type="checkbox">
-            <input type="hidden" name="action" value="view_results"/>
-            <br>
-            <input type="submit" value="Go" class='btn'>
-        </form>
+        <span id="contentwrapper">
+            <img src='/media/images/brand_glass.png'>        
+            <h1>FoodFinder</h1>
+            <form method="post">
+                <label class="icon">Pizza</label><input type="checkbox">
+                <label class="icon">Pop</label><input type="checkbox">
+                <label class="icon">Sugar</label><input type="checkbox">
+                <label class="icon">Soda</label><input type="checkbox">
+                <input type="hidden" name="action" value="view_results"/>
+                <br>
+                <input type="submit" value="Go" class='btn'>
+            </form>
+        </span>
     </body>
 </html>
