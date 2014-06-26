ef692d164d26d7eac709c8608ef3e41598a3bbbc
Bodily-Brad <bod12002@byui.edu>
2014-06-25 19:23:57 -0600

    Silly little icon toggle Not finished, but the guts are there

--- ffinder/views/view_survey.php
+++ ffinder/views/view_survey.php
@@ -9,14 +9,27 @@
             <img src='/media/images/brand_glass.png'>        
             <h1>FoodFinder</h1>
             <form method="post">
-                <label class="icon">Pizza</label><input type="checkbox">
-                <label class="icon">Pop</label><input type="checkbox">
-                <label class="icon">Sugar</label><input type="checkbox">
-                <label class="icon">Soda</label><input type="checkbox">
+                <label class="icon" onclick="toggleIcon(this)">Pizza</label><input type="checkbox">
+                <label class="icon" onclick="toggleIcon(this)">Pop</label><input type="checkbox">
+                <label class="icon" onclick="toggleIcon(this)">Sugar</label><input type="checkbox">
+                <label class="icon" onclick="toggleIcon(this)">Soda</label><input type="checkbox">
                 <input type="hidden" name="action" value="view_results"/>
                 <br>
                 <input type="submit" value="Go" class='btn'>
             </form>
+            <script>
+                function toggleIcon(clicker)
+                {
+                    var status = clicker.getAttribute("status");
+                    if (status === null)
+                        status = "off";                  
+                    window.alert(status);
+                    if (status == "off")
+                        clicker.setAttribute("status", "on");
+                    else
+                        clicker.setAttribute("status", "off");
+                }
+            </script>
         </span>
     </body>
 </html>
