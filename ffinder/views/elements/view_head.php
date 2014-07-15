37c0fee82a82a5c1f33f46a8a626678689614734
Brad <Brad@TAB-BOD-BRAD>
2014-07-15 09:23:36 -0600

    Added googleapis link to common head file

--- ffinder/views/elements/view_head.php
+++ ffinder/views/elements/view_head.php
@@ -1,6 +1,7 @@
 <head>
     <meta charset="UTF-8">
     <link rel="stylesheet" type="text/css" href="/style/ffstyle.css"/>
+    <script src="https://maps.googleapis.com/maps/api/js?libraries=places"></script>
     <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
     <script src="/models/mapAPI.js"></script>
     <title>Food Finder</title>
